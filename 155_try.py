0280ddffabda36d29e1ab6924973ca26141d45db
Vamse Kumar <vamse.kumar.subbiah@pwc.com>
2021-01-25 13:58:14 -0600

    init

--- 155_try.py
+++ 155_try.py
@@ -1,37 +1,38 @@
-class MinStack:
+# class MinStack:
 
-    def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        self.top = None
-        self.count = 0
-        self.minimum = 0
+#     def __init__(self):
+#         """
+#         initialize your data structure here.
+#         """
+#         self.top = None
+#         self.count = 0
+#         self.minimum = 0
         
 
-    def push(self, x: int) -> None:
-        self.top = x
-        self.count =+ 1
-        if (self.minimum > x):
-            self.minimum = x
-        return(self.append(x))
+#     def push(self, x: int) -> None:
+#         self.top = x
+#         self.count =+ 1
+#         if (self.minimum > x):
+#             self.minimum = x
+#         return(self.append(x))
         
 
-    def pop(self) -> None:
-        y = len(self)
-        z=self[y]
-        self.remove(y)
-        return(self)
-        
-
-    def top(self) -> int:
-        return(self[0])
+#     def pop(self) -> None:
+#         y = len(self)
+#         z=self[y]
+#         self.remove(y)
+#         return(self)
         
         
 
-    def getMin(self) -> int:
-        self.sort()
-        return(self[0])
+#     def top(self) -> int:
+#         return(self[0])
+        
+        
+
+#     def getMin(self) -> int:
+#         self.sort()
+#         return(self[0])
         
         
 
